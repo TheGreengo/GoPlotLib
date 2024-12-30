16920d068028bcbf9695578835eb1b1d434ded8a
Braden <bradenoptimus@gmail.com>
2024-12-30 16:19:16 -0700

    line drawing

--- main.go
+++ main.go
@@ -1,22 +1,63 @@
 package main
 
 import (
-    "fmt"
-    "github.com/TheGreengo/GoPlotLib/Canvas"
-    "github.com/TheGreengo/GoPlotLib/Saver"
-    "os"
+	"os"
+	"image"
+	"image/color"
+	"image/png"
 )
 
+/*
+Fun ideas:
+- draw axis
+- draw lines
+- set color
+*/
+
+// t = thickness of the line
+func draw_line(img *image.RGBA, x1 int, y1 int, x2 int, y2 int, t int, col color.RGBA) {
+    // so somehow we need to get all of the
+    steps := 500
+    dx := float64(x2-x1) / float64(steps)
+    dy := float64(y2-y1) / float64(steps)
+
+    for j := 0; j < t; j++ {
+        x := float64(x1)
+        y := float64(y1)
+        for i := 0; i < steps; i++ {
+            x += dx
+            y += dy
+            img.Set(int(x)+j, int(y), color.RGBA{0, 255, 0, 255})
+        }
+    }
+}
+
 func main() {
-    fmt.Println("GoPlotLib test started!")
-    can := canvas.NewCanvas(4,4)
-    fmt.Println(can)
+    farve := color.RGBA{255, 255, 255, 255}
+	// Create a new image with a specific width and height
+	width  := 640
+	height := 480
+	img    := image.NewRGBA(image.Rect(0, 0, width, height))
 
-    file, err := os.Create("temp.png")
-    if (err != nil) { panic(err) }
+	// Fill the image with red color
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.Set(x, y, farve)
+		}
+	}
 
-    png_saver.MakeHeader(file)
-    png_saver.MakeIEND(file)
-    
-    file.Close()
+    draw_line(img, 10, 10, 100, 400, 4, farve)
+
+	// Create a file to save the image
+	file, err := os.Create("output.png")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Encode the image as PNG and write to file
+	err = png.Encode(file, img)
+	if err != nil {
+		panic(err)
+	}
 }
