82c612eb4a8bc8bb7a10af7f3e00ebaaf8839ad9
Braden <bradenoptimus@gmail.com>
2025-01-02 23:04:31 -0700

    colurs

--- main.go
+++ main.go
@@ -10,9 +10,40 @@ import (
 /*
 Fun ideas:
 - draw axis
-- draw lines
-- set color
+- label graph
+- label axes
+- make points floats
 */
+
+var (
+    RED     color.RGBA = color.RGBA{ 255,   0,   0, 255 } // done
+    ORANGE  color.RGBA = color.RGBA{ 255, 165,   0, 255 } // done
+    YELLOW  color.RGBA = color.RGBA{ 255, 255,   0, 255 } // done
+    FORREST color.RGBA = color.RGBA{  34, 139,  34, 255 } // done
+    GREEN   color.RGBA = color.RGBA{ 175, 255,  47, 255 } // done
+    BLUE    color.RGBA = color.RGBA{   0,   0, 255, 255 } // done
+    PURPLE  color.RGBA = color.RGBA{ 148,   0, 211, 255 } // done
+    MAGENTA color.RGBA = color.RGBA{ 255,   0, 255, 255 } // done
+    WHITE   color.RGBA = color.RGBA{ 255, 255, 255, 255 } // done
+    GRAY    color.RGBA = color.RGBA{ 211, 211, 211, 255 } // done
+    BLACK   color.RGBA = color.RGBA{   0,   0,   0, 255 } // done
+    BROWN   color.RGBA = color.RGBA{ 139,  69,  19, 255 } // done
+    PINK    color.RGBA = color.RGBA{ 255,  20, 147, 255 } // done
+    S_BLUE  color.RGBA = color.RGBA{   0, 191, 255, 255 } // done
+    SEETHR  color.RGBA = color.RGBA{ 255, 255, 255, 255 } // done
+)
+
+func draw_rect() {
+}
+
+func draw_circle() {
+}
+
+func draw_rrect() {
+}
+
+func draw_slice() {
+}
 
 // t = thickness of the line
 func draw_line(img *image.RGBA, x1 int, y1 int, x2 int, y2 int, t int, col color.RGBA) {
@@ -27,13 +58,18 @@ func draw_line(img *image.RGBA, x1 int, y1 int, x2 int, y2 int, t int, col color
         for i := 0; i < steps; i++ {
             x += dx
             y += dy
-            img.Set(int(x)+j, int(y), color.RGBA{0, 255, 0, 255})
+            img.Set(int(x)+j, int(y), col)
         }
     }
 }
 
+func make_plot(img *image.RGBA) {
+    //dx := img.Bounds().Dx()
+    dy := img.Bounds().Dy()
+    draw_line(img, 5, 5, 5, dy - 5, 2, color.RGBA{0, 0, 0, 255})
+}
+
 func main() {
-    farve := color.RGBA{255, 255, 255, 255}
 	// Create a new image with a specific width and height
 	width  := 640
 	height := 480
@@ -42,11 +78,11 @@ func main() {
 	// Fill the image with red color
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			img.Set(x, y, farve)
+			img.Set(x, y, WHITE)
 		}
 	}
 
-    draw_line(img, 10, 10, 100, 400, 4, farve)
+    draw_line(img, 100, 100, 600, 200, 4, GREEN)
 
 	// Create a file to save the image
 	file, err := os.Create("output.png")
